18da92c7ebc96cf014c1c603cd9d67eded7fe34b
Gabriel260 <adeilmazk@gmail.com>
2021-02-01 19:33:30 +0000

    Initial commit: drop fixed sepolicy

---
+++ sepolicy.mk
@@ -0,0 +1,23 @@
+#
+# This policy configuration will be used by samsung products
+#
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/common/vendor
+
+ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),teegris)
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung_slsi/sepolicy/tee/teegris/public
+
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/tee/teegris/vendor
+else ifeq ($(BOARD_SEPOLICY_TEE_FLAVOR),mobicore)
+BOARD_SEPOLICY_DIRS += \
+    device/samsung_slsi/sepolicy/tee/mobicore
+endif
